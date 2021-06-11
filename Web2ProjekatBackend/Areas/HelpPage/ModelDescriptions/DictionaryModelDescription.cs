26308eb71a0ea3c4ff1477049c62db64396e76fb
= <ivan.grubor@gmail.com>
2021-06-11 20:29:05 +0200

    Add project files.

---
+++ Web2ProjekatBackend/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Web2ProjekatBackend.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Web2ProjekatBackend/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Web2ProjekatBackend.Startup))]
+
+namespace Web2ProjekatBackend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
