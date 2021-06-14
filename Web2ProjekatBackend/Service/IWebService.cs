a1ad210d897b93a816710ab7c36ed3b8eb3c36b0
nejvej@gmail.com <nejvej@gmail.com>
2021-06-14 10:56:16 +0000

    added stuff

---
+++ Web2ProjekatBackend/Service/IWebService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web2ProjekatBackend.Models;
+
+namespace Web2ProjekatBackend.Service
+{
+    public interface IWebService
+    {
+        bool addEntity(object entity);
+        List<object> getEntities(TipEntiteta tip);
+        bool updateEntity(object entity);
+        bool deleteEntity(object entity);
+        object getEntity(TipEntiteta tip, string id);
+    }
+}
