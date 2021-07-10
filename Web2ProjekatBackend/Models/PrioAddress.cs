9cded5cbab9ea1b0d46824efe830ed5219fed595
nejvej@gmail.com <nejvej@gmail.com>
2021-07-10 16:28:39 +0000

    Dodat prioaddress

---
+++ Web2ProjekatBackend/Models/PrioAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Web2ProjekatBackend.Models
+{
+    [Table("PrioAddress")]
+    public class PrioAddress
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string address { get; set; }
+        [Required]
+        public int prio { get; set; }
+    }
+}
